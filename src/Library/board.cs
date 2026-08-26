1a3e3c690d84b2710544900b114eeadbd0fb2b55
Facundo Rodriguez <facurodrisena@gmail.com>
2026-08-26 10:29:29 -0300

    Implementación clase Board

---
+++ src/Library/board.cs
@@ -0,0 +1,22 @@
+public class Board
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get { return width; }
+    }
+
+    public int Height
+    {
+        get { return height; }
+    }
+
+    public Board(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+}
