0ed6b109c6c2d17ee43303b412c10eca136ba07a
AugustoPedetti <augustopedettimartinez1@gmail.com>
2026-08-31 08:22:36 -0300

    Update boardprinter.cs

--- src/Library/boardprinter.cs
+++ src/Library/boardprinter.cs
@@ -1,42 +1,4 @@
-using System;
-using System.Text;
-using System.Threading;
 
-
-public class BoardPrinter
-{
-    public BoardPrinter()
-    {
-        
-    }
-
-    public void PrintBoard(Board board)
-    {
-        bool[,] b; // Variable que representa el tablero
-        while (true)
-    {
-        Console.Clear();
-        StringBuilder s = new StringBuilder();
-        for (int y = 0; y<board.Height;y++)
-        {
-            for (int x = 0; x<board.Width; x++)
-            {
-                if(b[x,y])
-                {
-                    s.Append("|X|");
-                }
-                else
-                {
-                    s.Append("___");
-                }
-            }
-            s.Append("\n");
-        }
-        Console.WriteLine(s.ToString());
-        //=================================================
-        //Invocar método para calcular siguiente generación
-        //=================================================
-        Thread.Sleep(300);
     }
     }
 }
